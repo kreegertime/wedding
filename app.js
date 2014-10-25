16a7f390479caceb1569a8e3933e503251ee46fa
Nick Kreeger <kreeger@strava.com>
2014-10-25 14:00:08 -0700

    Add hapi

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 var Hapi = require('hapi');
+var Path = require('path');
 var server = new Hapi.Server(3000);
+
+server.views({
+  engines: {
+    html: require('handlebars')
+  },
+  path: Path.join(__dirname, 'templates')
+});
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function(request, reply) {
+    reply.view('index');
+  }
+});
 
 server.start(function() {
   console.log('Server running at:', server.info.uri);
